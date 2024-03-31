2922c21b11f71a20c0b33419b78ea01eb00427ef
sanjibroy360 <sanjibroy0098@gmail.com>
2024-04-01 04:02:12 +0530

    Use "astro-compress" package to compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import rehypePrettyCode from "rehype-pretty-code";
 import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 // import { astroOgImagesGenerator } from "og-images-generator/astro"; // Install "og-images-generator" npm package then use it to generate opengraph image
+import compress from "astro-compress";
 
 const prettyCodeOptions = {
   theme: "dark-plus",
@@ -61,5 +62,6 @@ export default defineConfig({
     }),
     astroImageTools,
     // astroOgImagesGenerator(),
+    compress(),
   ],
 });
